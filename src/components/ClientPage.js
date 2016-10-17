053ef517212af28a17972dcaf706d894752f5444
dd di cesare <didi@posteo.net>
2016-10-17 12:10:38 +0200

    [wip][client] edit client

--- src/components/ClientPage.js
+++ src/components/ClientPage.js
@@ -3,6 +3,9 @@ import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card'
 import FlatButton from 'material-ui/FlatButton'
+import TextField from 'material-ui/TextField'
+import SelectField from 'material-ui/SelectField'
+import MenuItem from 'material-ui/MenuItem'
 import * as clientActions from '../actions/clientActions'
 
 
@@ -11,8 +14,12 @@ class ClientPage extends React.Component {
     super(props, context)
     this.state = {
       client: Object.assign({}, props.client),
-      errors: {}
+      errors: {},
+      edit: false
     }
+
+    this.setEditMode = this.setEditMode.bind(this)
+    this.updateClientState = this.updateClientState.bind(this)
   }
 
   componentWillReceiveProps (nextProps) {
@@ -21,26 +28,72 @@ class ClientPage extends React.Component {
     }
   }
 
+  setEditMode () {
+    this.setState({edit: true})
+  }
+
+  updateClientState (event) {
+    const field = event.target.name
+    let client = this.state.client
+    client[field] = event.target.value
+    return this.setState({client})
+  }
 
   render () {
     let client = this.state.client
-    return (
-      <Card>
-        <CardHeader
-          title={client.name}
-          subtitle={client.type}
-          avatar="/images/avatar.jpeg"
-        />
-        <CardTitle title="Card title" subtitle="Card subtitle" />
-        <CardText>
-          {client.description}
-        </CardText>
-        <CardActions>
-          <FlatButton label="Action1" />
-          <FlatButton label="Action2" />
-        </CardActions>
-      </Card>
-    )
+    let edit = this.state.edit
+    console.log(client)
+    if (edit) {
+      return (
+        <Card>
+          <TextField
+            name="name"
+            value={client.name}
+            hintText="Client's name"
+          /><br />
+          <TextField
+            name="surname"
+            value={client.surname}
+            hintText="Client's surname"
+          /><br />
+          <SelectField value={client.type} onChange={this.updateClientState}>
+            <MenuItem value="owner" primaryText="Owner" />
+            <MenuItem value="tenant" primaryText="Tenant" />
+            <MenuItem value="buyer" primaryText="Buyer" />
+          </SelectField>
+          <br />
+          <TextField
+            name="description"
+            value={client.description}
+            hintText="Client's description"
+            multiLine={true}
+            rows={2}
+            rowsMax={4}
+          />
+          <CardActions>
+            <FlatButton label="< Back" href={`/client/${client.id}`} />
+          </CardActions>
+        </Card>
+      )
+    } else {
+      return (
+        <Card>
+          <CardHeader
+            title={client.name}
+            subtitle={client.type}
+            avatar="/images/avatar.jpeg"
+          />
+          <CardTitle title="Card title" subtitle="Card subtitle" />
+          <CardText>
+            {client.description}
+          </CardText>
+          <CardActions>
+            <FlatButton label="< Back" href="/clients" />
+            <FlatButton label="Edit" onClick={this.setEditMode} />
+          </CardActions>
+        </Card>
+      )
+    }
   }
 }
 
