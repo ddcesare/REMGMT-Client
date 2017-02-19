abbd7bcc84dacaf0486dcd5dee9a219eea80ba40
dd di cesare <didi@posteo.net>
2017-02-19 21:31:00 +0100

    [mock api] shorter descriptions
    for the sake of testing

--- src/api/mockClientApi.js
+++ src/api/mockClientApi.js
@@ -11,9 +11,7 @@ const clients = [
     birthdate: '',
     email: 'george@best.ie',
     type: 'owner',
-    info: `Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular 
-      gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the 
-      most popular gun in American crime. Like they're actually proud of that shit.`
+    info: `Now that there is the Tec-9, a crappy spray gun from South Miami...`
   },
   {
     id: '2',
@@ -22,10 +20,7 @@ const clients = [
     birthdate: '',
     email: 'andrea@pirlo.it',
     type: 'tenant',
-    info: `slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a 
-      blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? 
-      No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here 
-      shopping for an infant named Jeb.`
+    info: `slender plastic tag clipped to my shirt with my name printed on it?...`
   },
   {
     id: '3',
@@ -34,9 +29,7 @@ const clients = [
     birthdate: '',
     email: 'zinadine@zidane.fr',
     type: 'buyer',
-    info: `Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone
-      into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better 
-      paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?`
+    info: `Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house...`
   }
 ]
 
